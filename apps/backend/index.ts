e8e1466f5ccec0ecb7395907908ef2930e7ab71a
Ram Gavhane <ramgavhane28@gmail.com>
2025-11-27 21:10:16 +0530

    scripts changes

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.get("/",(req, res)=>{
   res.json({
-    message:"Backend is Up ✅"
+    message:"Backend is Up ✅ \n Deployed using docker conatainer via CI/CD on AWS"
   })
 })
 
